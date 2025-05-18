3418d47ed1fdc732e9bb366fc828f28aea9583a3
Nguyễn Mỹ Duyên <duyenman19@gmail.com>
2025-05-19 00:19:21 +0700

    update

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const branchNames = {
+  BVD: 'Bến Vân Đồn',
+  GV: 'Gò Vấp',
+  XVNT: 'Xô Viết Nghệ Tĩnh',
+  LPM: 'Lý Phục Man',
+  TANKIENG: 'Tân Kiểng'
+};
+
+// Trang menu chính
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Trang quản lý chi nhánh' });
+});
+
+// Route động theo tên chi nhánh ngay tại gốc URL
+router.get('/:branch', function (req, res, next) {
+  const branch = req.params.branch.toUpperCase();
+
+  if (!branchNames[branch]) {
+    return res.status(404).render('error', { message: 'Chi nhánh không tồn tại', error: {} });
+  }
+
+  res.render('chi_nhanh_form', {
+    title: branchNames[branch],
+    branch: branch
+  });
 });
 
 module.exports = router;
